e5fb49c1fa85d4d4122a745fd7bb18430afe08f0
Mid1i <michac.tchernov@yandex.ru>
2024-12-02 17:04:27 +0300

    feat: Добавлен компонент модального окна сортировки

---
+++ src/components/SortingPopup.vue
@@ -0,0 +1,100 @@
+<script setup lang="ts">
+	import type { TypeSorting } from "@/types/TypeSorting";
+
+	import BaseCheckbox from "@/components/BaseCheckbox.vue";
+	import TheBlackout from "@/layouts/TheBlackout.vue";
+	import ThePopup from "@/layouts/ThePopup.vue";
+
+
+	defineProps<{
+		activeSorting: string;
+		sortingItems: TypeSorting[];
+		isVisible: boolean;
+	}>();
+
+	defineEmits<{
+		(e: "closePopup"): void,
+		(e: "setSorting", value: TypeSorting): void
+	}>();
+</script>
+
+
+<template>
+	<TheBlackout 
+		@close-popup="$emit('closePopup')"
+		:is-visible="isVisible"
+	>
+		<ThePopup
+			@close-popup="$emit('closePopup')"
+			:is-visible="isVisible"	
+			title="Сортировать пользователей"
+		>
+			<main class="content">
+				<ul class="content__list">
+					<li
+						v-for="(sorting) in sortingItems" 
+						:key="sorting.value"
+						class="content__list-el"
+					>
+						<BaseCheckbox
+							@click="$emit('setSorting', sorting.value)"
+							:isActive="sorting.value === activeSorting"
+							:id="sorting.value"
+						/>
+						<label :for="sorting.value" class="content__list-label">
+							<svg class="content__list-icon" height="14" width="14">
+								<use :xlink:href="`src/assets/icons/sorting.svg#${sorting.icon}`"/>
+							</svg>
+							{{ sorting.title }}
+						</label>
+					</li>
+				</ul>
+			</main>
+		</ThePopup>
+	</TheBlackout>
+</template>
+
+
+<style scoped lang="scss">
+	@use "@/assets/styles/variables.scss" as *;
+	@use "@/assets/styles/mixins.scss" as *;
+
+
+	.content {
+		flex: 1 0 auto;
+
+		&__list {
+			display: flex;
+			flex-direction: column;
+			gap: 1vw;
+
+			&-el {
+				align-items: center;
+				display: flex;
+				gap: 1vw;
+			}
+
+			&-icon:deep(path) {
+				fill: $text-primary;
+			}
+
+			&-label {
+				align-items: center;
+				display: flex;
+				gap: 0.8vw;
+
+				color:$text-primary;
+				font-weight: 700;
+				font-size: 0.8vw;
+				cursor: pointer;
+			}
+		}
+	}
+
+
+	@media(hover: hover) {
+		.content__list-el:hover .checkbox {
+			border-color: $text-secondary;
+		}
+	}
+</style>
