c4737470afefe6b3cce8aa173901f87ef69881ff
Mid1i <michac.tchernov@yandex.ru>
2024-12-02 17:05:13 +0300

    feat: Добавлена функция поиска нужного окончания слова

---
+++ src/helpers/words.ts
@@ -0,0 +1,12 @@
+export const getWordEnding = (amount: number, one: string, two: string, five: string): string => {
+	const exceptionsCondition: boolean = amount === 11 || amount === 12 || amount === 13;
+
+	const lastDigit: number = amount % 10;
+
+	if (lastDigit === 1 && !exceptionsCondition) return `${amount} ${one}`;
+	if ((lastDigit === 2 || lastDigit === 3 || lastDigit === 4) && !exceptionsCondition) return `${amount} ${two}`;
+
+	return `${amount} ${five}`;
+}
+
+export const addLeadingZeros = (num: number): string => num.toString().padStart(2, "0");
