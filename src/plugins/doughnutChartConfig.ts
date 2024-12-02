7d7a44b55c883c47397b985ec2b7438cd105f705
Mid1i <michac.tchernov@yandex.ru>
2024-12-02 17:08:01 +0300

    feat: Добавлен конфиг для настройки кругового графика

---
+++ src/plugins/doughnutChartConfig.ts
@@ -0,0 +1,59 @@
+import { ChartConfiguration, TooltipItem } from "chart.js";
+import type { TypeThemes } from "@/types/TypeThemes";
+
+
+export const chartConfig = (data: number[], labels: string[], colors: string[], theme: TypeThemes): ChartConfiguration => ({
+	type: "doughnut",
+	data: {
+		labels: labels,
+		datasets: [{
+			label: "",
+			data: data,
+			backgroundColor: colors,
+			hoverBackgroundColor: [...colors.map(item => `${item.slice(0, -1)}, .9)`)],
+			borderWidth: 0
+		}]
+	},
+	options: {
+		maintainAspectRatio: false,
+		responsive: true,
+		plugins: {
+			legend: {
+				display: false
+			},
+			tooltip: {
+				cornerRadius: 10,
+				displayColors: false,
+				titleAlign: "center",
+				titleMarginBottom: 10,
+				bodyFont: {
+					family: "Manrope",
+					size: 16,
+					weight: "bold",
+				},
+				bodyAlign: "center",
+				padding: {
+					y: 5,
+					x: 10,
+				},
+				callbacks: {
+					label: (context: TooltipItem<"doughnut">) => `${context.formattedValue} %`
+				},
+				titleColor: theme === "light" ? "#363949" : "#EDEFFD",
+				titleFont: {
+					family: "Manrope",
+					size: 14,
+					weight: "normal",
+				},
+				footerColor: theme === "light" ? "#363949" : "#EDEFFD",
+				footerAlign: "center",
+				footerMarginTop: 20,
+				footerFont: {
+					family: "Manrope",
+					size: 14,
+					weight: "normal",
+				}
+			}
+		}
+	} 
+})
