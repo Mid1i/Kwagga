1b3db66fefb36e756191c6c1e7156653c52a904f
Mid1i <michac.tchernov@yandex.ru>
2024-12-03 18:01:51 +0300

    feat: Добавлен хук для сортировки

---
+++ src/hooks/useSorting.ts
@@ -0,0 +1,35 @@
+import { ref, Ref } from "vue";
+
+
+export const useSorting = (page: Ref<number>) => {
+	/**
+	 * Текущая сортировка.
+	 */
+	const sorting = ref<string>("");
+
+	/**
+	 * Установить/сбросить сортировку.
+	 * 
+	 * @param {string} value - Текущая сортировка. 
+	 * @return {void}
+	 */
+	const setSorting = (value: string): void => {
+		sorting.value = sorting.value === value ? "" : value;
+		page.value = 0;
+	};
+
+
+	return {
+		/**
+		 * Текущая сортировка.
+		 */
+		sorting,
+		/**
+		 * Установить/сбросить сортировку.
+		 * 
+		 * @param {string} value - Текущая сортировка. 
+		 * @return {void}
+		 */
+		setSorting
+	}
+}
