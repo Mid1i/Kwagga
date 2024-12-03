6c9de2233b7f1b18452b4e6f562b1504d5f4af52
Mid1i <michac.tchernov@yandex.ru>
2024-12-03 18:01:37 +0300

    feat: Добавлен хук для пагинации

---
+++ src/hooks/usePagination.ts
@@ -0,0 +1,43 @@
+import { ref } from "vue";
+
+
+export const usePagination = () => {
+	/**
+	 * Кол-во броней, отображаемых на странице в пагинации.
+	 */
+	const PAGINATION_SIZE = 11;
+
+	/**
+	 * Текущая страница пагинации.
+	 */
+	const page = ref<number>(0);
+
+	/**
+	 * Изменение страницы пагинации.
+	 * 
+	 * @param {number} newPage - Новая страница. 
+	 * @return {void}
+	 */
+	const updatePage = (newPage: number): void => {
+		page.value = newPage;
+	};
+
+
+	return {
+		/**
+		 * Текущая страница пагинации.
+		 */
+		page,
+		/**
+		 * Кол-во броней, отображаемых на странице в пагинации.
+		 */
+		PAGINATION_SIZE,
+		/**
+		 * Изменение страницы пагинации.
+		 * 
+		 * @param {number} newPage - Новая страница. 
+		 * @return {void}
+		 */
+		updatePage
+	}
+}
