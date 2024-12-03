d5f7b732bdb5408633b8ce265428eb308823cc6c
Mid1i <michac.tchernov@yandex.ru>
2024-12-03 18:05:58 +0300

    fix: Добавлены новые константы

--- src/constants.ts
+++ src/constants.ts
@@ -17,42 +17,90 @@ export const USERS_COLORS: TypeChartColors = {
 export const SORTING_ITEMS: TypeSorting[] = [
 	{
 		icon: "alphabet-asc",
-		title: "По имени (A-Z)",
+		title: "По имени",
+		htmlTitle: "Сортировать по имени от A до Z",
 		value: "firstName,asc"
 	},
 	{
 		icon: "alphabet-desc",
-		title: "По имени (Z-A)",
+		title: "По имени",
+		htmlTitle: "Сортировать по имени от Z до A",
 		value: "firstName,desc"
 	},
 	{
 		icon: "alphabet-asc",
-		title: "По фамилии (A-Z)",
+		title: "По фамилии",
+		htmlTitle: "Сортировать по фамилии от A до Z",
 		value: "lastName,asc"
 	},
 	{
 		icon: "alphabet-desc",
-		title: "По фамилии (Z-A)",
+		title: "По фамилии",
+		htmlTitle: "Сортировать по фамилии от Z до A",
 		value: "lastName,desc"
 	},
 	{
 		icon: "alphabet-asc",
-		title: "По email (A-Z)",
+		title: "По email",
+		htmlTitle: "Сортировать по email от A до Z",
 		value: "email,asc"
 	},
 	{
 		icon: "alphabet-desc",
-		title: "По email (Z-A)",
+		title: "По email",
+		htmlTitle: "Сортировать по email от Z до A",
 		value: "email,desc"
 	},
 	{
 		icon: "asc",
-		title: "По дате регистрации (A-Z)",
+		title: "По дате регистрации",
+		htmlTitle: "Сортировать по дате регистрации от более ранних к более поздним",
 		value: "dateOfRegistration,asc"
 	},
 	{
 		icon: "desc",
-		title: "По дате регистрации (Z-A)",
+		title: "По дате регистрации",
+		htmlTitle: "Сортировать по дате регистрации от более поздних к более ранним",
 		value: "dateOfRegistration,desc"
 	}
 ];
+
+
+export const BOOKING_SORTING_ITEMS: TypeSorting[] = [
+	{
+		icon: "alphabet-asc",
+		title: "По пользователю",
+		htmlTitle: "Сортировать по email пользователя от A до Z",
+		value: "email,asc"
+	},
+	{
+		icon: "alphabet-desc",
+		title: "По пользователю",
+		htmlTitle: "Сортировать по email пользователя от Z до A",
+		value: "email,desc"
+	},
+	{
+		icon: "asc",
+		title: "По дате создания",
+		htmlTitle: "Сортировать по дате создания от более ранних к более поздним",
+		value: "dateOfCreating,asc"
+	},
+	{
+		icon: "desc",
+		title: "По дате создания",
+		htmlTitle: "Сортировать по дате создания от более поздних к более ранним",
+		value: "dateOfCreating,desc"
+	},
+	{
+		icon: "asc",
+		title: "По дате брони",
+		htmlTitle: "Сортировать по дате брони от более ранних к более поздним",
+		value: "dateOfBooking,asc"
+	},
+	{
+		icon: "desc",
+		title: "По дате брони",
+		htmlTitle: "Сортировать по дате брони от более поздних к более ранним",
+		value: "dateOfBooking,desc"
+	}
+]
