de64c15de38f710d312bd0231570644f636819b6
Mid1i <michac.tchernov@yandex.ru>
2024-12-04 22:38:01 +0300

    fix: Добавлено модальное окно аккаунта админа

--- src/layouts/TheHeader.vue
+++ src/layouts/TheHeader.vue
@@ -1,14 +1,21 @@
 <script setup lang="ts">
 	import { useRoute } from "vue-router";
 
+	import { usePopup } from "@/hooks/usePopup";
 	import { useTheme } from "@/store/theme";
 
 
 	const router = useRoute();
 
-	const username = "Midi";
+	const admin = {
+		firstName: "Михаил",
+		lastName: "Чернов",
+		email: "example@mail.ru"
+	};
 
 	const themeStore = useTheme();
+
+	const { isActivePopup, togglePopup } = usePopup();
 </script>
 
 
@@ -36,16 +43,21 @@
 					</svg>
 				</button>
 			</div>
-			<div class="header__account" title="Открыть аккаунт">
+			<div class="header__account">
 				<span class="header__account-text">
-					Добро пожаловать, <b>{{ username }}</b> <br/>
+					Добро пожаловать, <b>{{ admin.firstName }}</b> <br/>
 					Администратор
 				</span>
-				<button class="header__account-button">
+				<button @click="togglePopup" class="header__account-button" title="Открыть аккаунт">
 					<svg class="header__account-icon" height="24" width="24">
 						<use xlink:href="@/assets/icons/account.svg#account"/>
 					</svg>
 				</button>
+				<div @click="togglePopup" :class="['header__account-popup account', { active: isActivePopup }]">
+					<h6 class="account__title">{{ `${admin.firstName} ${admin.lastName}` }}</h6>
+					<span class="account__email">{{ admin.email }}</span>
+					<button class="account__button" title="Выйти из админ-панели">Выйти</button>
+				</div>
 			</div>
 		</div>
 	</header>
@@ -54,6 +66,7 @@
 
 <style scoped lang="scss">
 	@use "@/assets/styles/variables.scss" as *;
+	@use "@/assets/styles/mixins.scss" as *;
 
 
 	.header {
@@ -115,6 +128,8 @@
 			display: flex;
 			gap: 0.8vw;
 
+			position: relative;
+
 			&-text {
 				color: $text-secondary;
 				font-size: 0.7vw;
@@ -146,6 +161,50 @@
 		}
 	}
 
+	.account {
+		@include layout;
+
+		border-radius: 1vw;
+		
+		align-items: center;
+		padding: 1vw 2vw;
+
+		pointer-events: none;
+		opacity: 0;
+
+		position: absolute;
+		top: calc(100% + 0.5vw);
+		right: 0px;
+
+		z-index: 5;
+
+		&.active {
+			pointer-events: auto;
+			opacity: 1;
+		}
+
+		&__title {
+			color: $text-primary;
+			font-weight: 700;
+			font-size: 0.8vw;
+
+			margin-bottom: 0.25vw;
+		}
+
+		&__email {
+			color: $text-primary;
+			font-size: 0.7vw;
+
+			margin-bottom: 1vw;
+		}
+
+		&__button {
+			color: $text-primary;
+			font-weight: 700;
+			font-size: 0.8vw;
+		}
+	}
+
 
 	@media(hover: hover) {
 		.header__account-button:hover {
@@ -154,6 +213,10 @@
 			& .header__account-icon {
 				color: $accent-white;
 			}
+		}
+
+		.account__button:hover {
+			color: $accent-red;
 		}
 	}
 </style>
