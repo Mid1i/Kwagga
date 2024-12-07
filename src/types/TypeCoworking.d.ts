5b52ebdf44f0a2d457f124313d21fdcedc7dc7db
Mid1i <michac.tchernov@yandex.ru>
2024-12-07 22:46:33 +0300

    fix: Добавлен тип с данными по коворкинг-зоне

--- src/types/TypeCoworking.d.ts
+++ src/types/TypeCoworking.d.ts
@@ -6,3 +6,28 @@ export type TypeCoworkingPlace = {
 	id: number;
 	name: string;
 };
+
+export type TypeCoworkingSpaceExpanded = {
+	id: number;
+	active: boolean;
+	dateOfCreating: string;
+	dateOfUpdating: string;
+	coordinate: [number, number];
+	title: string;
+	description: string;
+	address: string;
+	images: string[];
+	conveniences: string[];
+	scheme: string;
+	places: TypeCoworkingPlaceExpanded[];
+};
+
+export type TypeCoworkingPlaceExpanded = {
+	id: number;
+	active: boolean;
+	dateOfCreating: string;
+	dateOfUpdating: string;
+	title: string;
+	spaceId: number;
+	capacity: number;
+};
