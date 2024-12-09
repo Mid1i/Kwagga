5c9b82c178191bbd74dab09fc59e0d61fb8456d6
Mid1i <michac.tchernov@yandex.ru>
2024-12-09 19:58:25 +0300

    fix: Добавлена прозрачная вариация инпута

--- src/components/BaseInput.vue
+++ src/components/BaseInput.vue
@@ -1,33 +1,64 @@
 <script setup lang="ts">
+	import { ref } from "vue";
+	import type { TypeDadata } from "@/types/TypeDadata";
+	
+	import { formatAddress } from "@/helpers/formatters";
+
+
 	defineProps<{
+		suggestions?: TypeDadata | null;
+		isTransparent?: boolean;
 		isReadonly?: boolean;
 		name: string;
 		text: string;
 		type: string;
 	}>();
 
+	defineEmits<{
+		applySuggestion: [address: string]
+	}>();
+
 
 	const localModel = defineModel();
+
+	const isFocus = ref<boolean>(false);
 </script>
 
 
 <template>
 	<div class="field">
 		<input
+			@focus="() => isFocus = !isFocus"
 			v-model="localModel"
-			class="field__input"
+			:class="['field__input', { transparent: isTransparent }]"
 			:readonly="!!isReadonly"
 			:name="name"
 			:type="type"
 			:id="name"
 		/>
 		<label class="field__label" :htmlFor="name">{{ text }}</label>
+
+		<!-- Дополнительная секция для инпута адреса с подсказками при вводе -->
+		<ul 
+			v-if="suggestions && suggestions.data.suggestions.length > 0"
+			:class="['field__suggestions', { active: isFocus }]"
+		>
+			<li 
+				v-for="suggest in suggestions.data.suggestions"
+				@click="$emit('applySuggestion', formatAddress(suggest))"
+				:key="`${suggest.data.house}-${suggest.data.flat}-${suggest.data.street_with_type}`"
+				class="field__suggestions-el"
+			>
+				{{ formatAddress(suggest)  }}
+			</li>
+		</ul>
 	</div>
 </template>
 
 
 <style scoped lang="scss">
 	@use "@/assets/styles/variables.scss" as *;
+	@use "@/assets/styles/mixins.scss" as *;
 
 
 	.field {
@@ -52,6 +83,35 @@
 			transform: translateY(-50%);
 		}
 
+		&__suggestions {
+			@include layout;
+			border-radius: 0px 0px 0.8vw 0.8vw;
+
+			gap: 0.8vw;
+
+			pointer-events: none;
+			opacity: 0;
+
+			position: absolute;
+			top: 100%;
+			left: 0px;
+
+			width: 100%;
+			z-index: 3;
+
+			&.active {
+				pointer-events: auto;
+				opacity: 1;
+			}
+
+			&-el {
+				color: $text-secondary;
+				cursor: pointer;
+				font-weight: 500;
+				font-size: 0.7vw;
+			}
+		}
+
 		&__input {
 			background: $background-secondary;
 			border: 0.05vw solid transparent;
@@ -65,6 +125,14 @@
 
 			height: 2.6vw;
 			width: 100%;
+
+			&.transparent {
+				border-color: $background-primary;
+
+				& + .field__label {
+					background: $background-secondary;
+				}
+			}
 
 			&:read-only {
 				pointer-events: none;
@@ -80,6 +148,10 @@
 	@media(hover: hover) {
 		.field__input:hover {
 			border-color: $accent-blue;
+		}
+
+		.field__suggestions-el:hover {
+			color: $text-primary;
 		}
 	}
 </style>
