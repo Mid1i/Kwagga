e555c5b75ef72ec81eab8f13ccf4a9bf4735bdf7
Mid1i <michac.tchernov@yandex.ru>
2024-12-09 19:59:39 +0300

    fix: Добавлено форматирование для адреса

--- src/helpers/formatters.ts
+++ src/helpers/formatters.ts
@@ -1,3 +1,5 @@
+import type { TypeDadataInformation } from "@/types/TypeDadata";
+
 import { addLeadingZeros } from "@/helpers/words";
 
 
@@ -33,3 +35,23 @@ export const formatMonth = (month: number): string => {
  * @return {string} Возвращает дату в формате "ДД.ММ.ГГГГ". 
  */
 export const formatDate = (date: Date): string => `${addLeadingZeros(date.getDate())}.${addLeadingZeros(date.getMonth() + 1)}.${addLeadingZeros(date.getFullYear())}`;
+
+/**
+ * Форматирование адреса в формат "Страна, округ, город, дом, квартира".
+ * 
+ * @param {TypeDadataInformation} suggest - Объект с сервиса Dadata, содержащий всю информацию об адресе. 
+ * @return {string} Возвращает адрес в формате "Страна, округ, город, дом, квартира".
+ */
+export const formatAddress = (suggest: TypeDadataInformation): string => {
+	const address = {
+		country: suggest.data.country ?? "",
+		region: suggest.data.region_with_type ?? "",
+		city: suggest.data.city_with_type ?? "",
+		street: suggest.data.street_with_type ?? "",
+		house: suggest.data.house ? `д ${suggest.data.house}` : "",
+		flat: suggest.data.flat ? `кв ${suggest.data.flat}` : ""
+	};
+
+
+	return Object.values(address).filter(Boolean).join(", ");
+};
